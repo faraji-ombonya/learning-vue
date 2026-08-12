8b761771fbe5472e2e8dada10774a5aa736a5c7a
Faraji Ombonya <farajiombonya@gmail.com>
2026-08-12 09:37:38 +0300

    declarative rendering

---
+++ Vue Tutorial/src/App2.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+import { reactive } from 'vue';
+
+
+// Reactive only works on objects including arrays and built
+// in types like Map and Set
+const counter = reactive({
+    count: 0
+})
+
+console.log(counter.count)
+counter.count++
+console.log(counter.count)
+
+// Ref can take any value type
+const message = ref("Hello world!")
+
+console.log(message.value)
+
+message.value = 'Changed'
+
+console.log(message.value)
+
+</script>
+
+<template>
+    <!-- We dont need to use .value, it is automatically unwrapped -->
+    <h1>{{ message }}</h1>
+
+    <!-- We can use any valid JavaScript expressions inside the moustache. -->
+    <h1>{{ message.split('').reverse().join('') }}</h1>
+    <p>Count is {{ counter.count }} </p>
+</template>
--- Vue Tutorial/src/main.js
+++ Vue Tutorial/src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import App2 from './App2.vue'
 
-createApp(App).mount('#app')
+createApp(App2).mount('#app')
