7cb4f1d58031075d4cbb9031ba64ee621b6de58a
Faraji Ombonya <farajiombonya@gmail.com>
2026-08-12 14:16:25 +0300

    slots

---
+++ Tutorial/tutorial/components/ChildComp.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <slot>Fall back content</slot>
+</template>
---
+++ Tutorial/tutorial/components/Slots.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from 'vue';
+import ChildComp from './ChildComp.vue';
+
+</script>
+
+<template>
+    <ChildComp>Message from parent</ChildComp>
+</template>
