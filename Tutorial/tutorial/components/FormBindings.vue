ca0a5513910fdd6e4d06fea9e5703a0af0cce02b
Faraji Ombonya <farajiombonya@gmail.com>
2026-08-12 11:25:34 +0300

    Form bindings

---
+++ Tutorial/tutorial/components/FormBindings.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+
+const text = ref('')
+
+function onInput(e) {
+    text.value = e.target.value
+}
+</script>
+
+<template>
+
+    <!-- We can use v-bind and v-on to create to-way bindings -->
+    <input :value="text" @input="onInput" placeholder="Type here" />
+
+    <p>{{ text }}</p>
+
+    <!-- We can use v-model to simplify two-way binding -->
+    <input v-model="text" />
+</template>
--- Tutorial/tutorial/src/App.vue
+++ Tutorial/tutorial/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import AttributeBindings from '../components/AttributeBindings.vue';
 import EventListeners from '../components/EventListeners.vue';
+import FormBindings from '../components/FormBindings.vue';
 </script>
 
 <template>
   <AttributeBindings />
   <EventListeners />
+  <FormBindings />
 </template>
 
 <style scoped></style>
