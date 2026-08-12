d9366fbe50d5c7f5b5702093540be402c35878f1
Faraji Ombonya <farajiombonya@gmail.com>
2026-08-12 11:18:57 +0300

    Add event listeners

---
+++ Tutorial/tutorial/components/EventListeners.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0)
+
+function increment() {
+    count.value++
+}
+</script>
+
+<template>
+    <!-- make this button work -->
+    <button @click="increment">Count is: {{ count }}</button>
+
+    <!-- We can listen to DOM evens using the v-on directive -->
+    <button v-on:click="increment">{{ count }}</button>
+
+    <!-- v-on also has a shorthand syntax - @ -->
+    <button @click="increment">{{ count }}</button>
+</template>
--- Tutorial/tutorial/src/App.vue
+++ Tutorial/tutorial/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import AttributeBindings from '../components/AttributeBindings.vue';
+import EventListeners from '../components/EventListeners.vue';
 </script>
 
 <template>
   <AttributeBindings />
+  <EventListeners />
 </template>
 
 <style scoped></style>
