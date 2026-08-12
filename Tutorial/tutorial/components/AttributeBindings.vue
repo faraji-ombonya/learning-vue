a38bf15cbe5acf228bc2423f6fa141f258169bd5
Faraji Ombonya <farajiombonya@gmail.com>
2026-08-12 11:08:44 +0300

    attribute bindings

---
+++ Tutorial/tutorial/components/AttributeBindings.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+const titleClass = ref('title')
+</script>
+<template>
+    <h1 :class="titleClass">Make me red</h1>
+
+    <!-- use v-bind directive to bind an attribute to a dynamic value -->
+    <div v-bind:id="dynamicId"></div>
+
+    <!-- v-bind has a dedicated shorthand syntax -->
+    <div :id="dynamicId2"></div>
+
+
+</template>
+<style>
+.title {
+    color: red;
+}
+</style>
