1f5fb794f42293c43ddd711010bd44c33cbeb666
Faraji Ombonya <farajiombonya@gmail.com>
2026-08-12 13:30:55 +0300

    computed property

--- Tutorial/tutorial/components/ListRendering.vue
+++ Tutorial/tutorial/components/ListRendering.vue
@@ -1,22 +1,36 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 let id = 0
 
 const newTodo = ref('')
 
+const hideCompleted = ref(false)
+
 const todos = ref([
-    { id: id++, text: "Learn HTML" },
-    { id: id++, text: "Learn JavaScript" },
-    { id: id++, text: "Learn Vue" }
+    { id: id++, text: "Learn HTML", done: true },
+    { id: id++, text: "Learn JavaScript", done: true },
+    { id: id++, text: "Learn Vue", done: false }
 ])
 
 function addTodo() {
-    todos.value.push({ id: id++, text: newTodo.value })
+    todos.value.push({ id: id++, text: newTodo.value, done: false })
 }
 
 function removeTodo(todo) {
     todos.value = todos.value.filter((element) => element.id != todo.id)
+}
+
+const filteredTodos = computed(() => {
+    if (hideCompleted.value) {
+        return todos.value.filter((todo) => !todo.done)
+    }
+
+    return todos.value
+})
+
+function toggleHideCompleted() {
+    hideCompleted.value = !hideCompleted.value
 }
 
 </script>
@@ -30,10 +44,17 @@ function removeTodo(todo) {
     </form>
 
     <ul>
-        <li v-for="todo in todos" :key="todo.id">
+        <li v-for="todo in filteredTodos" :key="todo.id">
+            <input type="checkbox" v-model="todo.done" />
             {{ todo.text }}
 
             <button @click="removeTodo(todo)">X</button>
         </li>
     </ul>
+
+
+    <button @click="toggleHideCompleted">
+        <p v-if="hideCompleted">Show all</p>
+        <p v-else>Hide completed</p>
+    </button>
 </template>
